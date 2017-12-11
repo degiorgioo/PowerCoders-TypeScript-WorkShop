1f775ee30209d7eefe56d0b6a38e7bbca1e0fd3c
degiorgioo <dave_degiorgio@hotmail.com>
2017-12-11 22:10:56 +0100

    Documentation added, printSumm3 Description changed  :memo:

--- JavaScript/printSum3.js
+++ JavaScript/printSum3.js
@@ -1,8 +1,26 @@
+/**
+ * printSum3 method
+ * In this method was tried to convert both the values in numbers.
+ * If values for summand1 and summand2 are both numbers, the result can be calculated.
+ * One or both of the given values could be another type than a number. When this occurs
+ * an Error message ("Invalid Type") is displayed. 
+ * 
+ * @param {number} summand1 - Number one for the calculation
+ * @param {number} summand2 - Number two for the calculation
+ * @returns {number} - Retruns the sum of the two numbers
+ */
 function printSum3(summand1,summand2) {
     var result = Number(summand1) + Number(summand2);
     console.log(result);
 }
 
+/**
+ * 1) Valid call.
+ * 2) Valid call, because "1" will be converted to a number.
+ * 3) Invalid call, because the Number-method can't convert the given values.
+ * --> If Number can't convert the values, the method return 'NaN'.
+ * Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number#Description 
+ */
 printSum3(1,2); // Result: 3
 printSum3("1", 2); // Result: 3
 printSum3("a", "b"); // Result: NaN
