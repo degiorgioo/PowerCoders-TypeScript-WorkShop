b034a084ed8598638007be970cd0393ead8aa85a
degiorgioo <dave_degiorgio@hotmail.com>
2017-12-11 22:04:55 +0100

    Documentation added, printSumm2 Description changed  :memo:

--- JavaScript/printSum2.js
+++ JavaScript/printSum2.js
@@ -1,3 +1,14 @@
+/**
+ * printSum2 method
+ * In this method was tried to check the type of the given values.
+ * If values for summand1 and summand2 are both numbers, the result can be calculated.
+ * One or both of the given values could be another type than a number. When this occurs
+ * an Error message ("Invalid Type") is displayed. 
+ * 
+ * @param {number} summand1 - Number one for the calculation
+ * @param {number} summand2 - Number two for the calculation
+ * @returns {number} - Retruns the sum of the two numbers
+ */
 function printSum2(summand1,summand2) {
     if (typeof summand1 == 'number'  && typeof summand2 == 'number') {
         var result = summand1 + summand2;
@@ -7,6 +18,11 @@ function printSum2(summand1,summand2) {
     }
 }
 
+/**
+ * 1) This method-call is valid, because all values are numbers.
+ * 2) The first value is not a number, thus the method prints the error-message.
+ * 3) Both values are invalid.
+ */
 printSum2(1,2); // Result: 3
 printSum2("1", 2); // Result: "Invalid Type"
 printSum2("a", "b"); // Result: "Invalid Type"
