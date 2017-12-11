1fe886ddf85b051f714614e1ddcc9d46e23e2cbf
degiorgioo <dave_degiorgio@hotmail.com>
2017-12-11 21:52:28 +0100

    Documentation added, printSum method described :memo:

--- JavaScript/printSum.js
+++ JavaScript/printSum.js
@@ -1,8 +1,19 @@
-
+/**
+ * printSum method
+ * @param {number} summand1 - Number one for the calculation
+ * @param {number} summand2 - Number two for the calculation
+ * @returns {number} - Retruns the sum of the two numbers
+ */
 function printSum(summand1,summand2) {
     var result = summand1 + summand2; 
     console.log(result);
 }
+
+/**
+ * the main problem of this method is, that it can be called with all every Object.
+ * The method tries to run the operator with every passed parameter. 
+ * Below you see an example for this situation.
+ */
 
 printSum(1,2); // Result: 3
 printSum("1", 2); // Result: 12
